f305d5c16b0a9741d3c3ff1599d4efc97e2ec4d9
Ernesto Aldo Tarrillo Perez <EA.TarrilloP@up.edu.pe>
2017-11-03 14:36:08 -0500

    Estructura Header Lista

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+$('.button-collapse').sideNav({
+      menuWidth: "90%", // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true
+ });
