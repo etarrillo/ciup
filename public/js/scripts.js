c329bf365c167134edd88e3661b7dd7ee58491b9
Ernesto Aldo Tarrillo Perez <EA.TarrilloP@up.edu.pe>
2017-11-03 22:07:16 -0500

    parte terminada sin la sección de noticias, estilos ya creados en las cajas

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 $('.button-collapse').sideNav({
-      menuWidth: "90%", // Default is 300
+      menuWidth: "80%", // Default is 300
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true
