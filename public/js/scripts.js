5ef08946b473bf103cf68911a77865a022813385
Ernesto Aldo Tarrillo Perez <EA.TarrilloP@up.edu.pe>
2017-11-09 15:45:57 -0500

    observacione de lusho

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,19 @@
 $('.button-collapse').sideNav({
-      menuWidth: "80%", // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true
- });
+menuWidth: "80%", // Default is 300
+edge: 'left', // Choose the horizontal origin
+closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+draggable: true
+});
 $('select').material_select();
-
+$('.modal').modal();
 // browser window scroll (in pixels) after which the "back to top" link is shown
 var offset = 300,
-	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-	offset_opacity = 1200,
-	//duration of the top scrolling animation (in ms)
-	scroll_top_duration = 700,
-	//grab the "back to top" link
-	$back_to_top = $('.cd-top');
+//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+offset_opacity = 1200,
+//duration of the top scrolling animation (in ms)
+scroll_top_duration = 700,
+//grab the "back to top" link
+$back_to_top = $('.cd-top');
 
 //hide or show the "back to top" link
 $(window).scroll(function(){
@@ -28,6 +28,10 @@ $back_to_top.on('click', function(event){
 	event.preventDefault();
 	$('body,html').animate({
 		scrollTop: 0 ,
-	 	}, scroll_top_duration
+	}, scroll_top_duration
 	);
 });
+
+
+
+
