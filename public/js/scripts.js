b4dacd0cb2ae7c5765d55957d041bfea02098ed4
GaliaPedroza <galiapedroza@gmail.com>
2017-11-21 18:26:07 -0500

    se agrego el label en los filtros, y los dias en español en el calendario

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -77,6 +77,7 @@ $(".datepicker").pickadate({
     monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
     weekdaysFull: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
     weekdaysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+    weekdaysLetter: [ 'D', 'L', 'M', 'X', 'J', 'V', 'S' ],
     showMonthsShort: undefined,
     showWeekdaysFull: undefined,
 
